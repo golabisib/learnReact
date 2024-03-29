7ecd82b78fd92921b4200e535dc045bf65c015b7
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-03-29 20:46:20 +0330

    first component is added

--- learningReact/src/App.jsx
+++ learningReact/src/App.jsx
@@ -1,10 +1,11 @@
-
+import Test from "./Test.jsx";
 function App() {
 
   return (
       <div>
         <h1>Roozbeh React</h1>
         <p>React project</p>
+        <Test />
     </div>
   )
 }
---
+++ learningReact/src/Test.jsx
@@ -0,0 +1,9 @@
+function Test(){
+    return(
+        <div>
+        <p>This is test component</p>
+        <span>I am Roozbeh Rigi Jangjo</span>
+        </div>
+    );
+}
+export default Test
