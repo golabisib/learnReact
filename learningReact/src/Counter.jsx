8ae7d11d241c4ce57ab1409f13f76af12bf068d5
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-04-01 23:02:01 +0330

    The useState part is added

--- learningReact/src/Counter.jsx
+++ learningReact/src/Counter.jsx
@@ -15,21 +15,43 @@
 // // without any external function
 // export default Counter
 
-// hook
+// hook & state
 import { useState } from "react";
 
 function Counter() {
     const [counter, setCounter] = useState(0);
-    const increaseHandler = () => setCounter(counter + 1);
+    const [isShow, setShow] = useState(true);
+
+    const increaseHandler = () => setCounter(counter => counter + 1);
+    const increaseHandler3 = () => {
+        setCounter(counter => counter + 1)
+        setCounter(counter => counter + 1)
+        setCounter(counter => counter + 1)
+    };
+    const stopHandler = () => setCounter(0);
+
+
+    const showHandler = () => setShow(true);
+    const hideHandler = () => setShow(false);
+
 
     return (
         <>
-        <h1>counter</h1>
-        <p>{counter}</p>
-        <button onClick={increaseHandler}>increase</button>
+        <button onClick={showHandler}>show</button>
+        <button onClick={hideHandler}>hide</button>
+        {
+            isShow && (
+            <div>
+                <h1>counter</h1>
+                <p>{counter}</p>
+                <button onClick={increaseHandler}>increase 1</button>
+                <button onClick={increaseHandler3}>increase 3</button>
+                <button onClick={stopHandler}>stop</button>
+            </div>
+            ) }
         </>
-  )
+        );
 }
-//46
+//50
 
 export default Counter
