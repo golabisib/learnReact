365bceee5fd56444c89b1ec6f7e65ee621b8a516
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-04-14 22:09:21 +0330

    The Error handling is added

--- ReactSideEffect/src/components/Users.jsx
+++ ReactSideEffect/src/components/Users.jsx
@@ -3,24 +3,30 @@ import { useEffect, useState } from "react"
 
 function Users() {
     const [users, setUsers] = useState([]);
+    const [error, setError] = useState(false);
 
   useEffect( () => {
     async function fetchUsers() {
-        const response = await fetch("https://jsonplaceholder.typicode.com/users")
+        try {
+        const response = await fetch("htps://jsonplaceholder.typicode.com/users")
         const json = await response.json();
         setUsers(json);
+    } catch(error) {
+        setError(true);
+    }
     }
     fetchUsers();
   }, []);
 
   return (
     <div>
-        {!users.length && <h3>loading...</h3>}
+        {!users.length && !error && <h3>loading...</h3>}
         <ul>
             {users.map((user) => (
                 <li key={user.id}>{user.name}</li>
             ))}
         </ul>
+        {error && <>Something went wrong!</>}
     </div>
   )
 }
