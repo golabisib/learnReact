995c6edbceaff666e65fd0382f8edded9fc7ebd4
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-05-03 23:43:24 +0330

    The Interceptors for response is added

--- Axios/src/main.jsx
+++ Axios/src/main.jsx
@@ -15,6 +15,17 @@ axios.interceptors.request.use(
     }
 )
 
+axios.interceptors.response.use(
+    (response) => {
+        console.log(response)
+        return response.data
+    },
+    (error) => {
+        console.log(error)
+        return Promise.reject(error)
+    }
+)
+
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<App />
