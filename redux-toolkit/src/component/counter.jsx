7c4d9a3483c11210eb1eada28182733f20244191
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-05-18 20:10:13 +0330

    The useDispatch hook from react-redux is added

--- redux-toolkit/src/component/counter.jsx
+++ redux-toolkit/src/component/counter.jsx
@@ -1,12 +1,23 @@
-import { useSelector } from "react-redux";
-import { selectCounter } from "../features/counter/counterSlice";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  decrement,
+  increment,
+  selectCounter,
+} from "../features/counter/counterSlice";
 
 function Counter() {
   const counter = useSelector(selectCounter);
+  const dispatch = useDispatch();
   return (
     <div>
       counter
       <h2>counter is {counter}</h2>
+      <button type="button" onClick={() => dispatch(increment())}>
+        Increment
+      </button>
+      <button type="button" onClick={() => dispatch(decrement())}>
+        Decrement
+      </button>
     </div>
   );
 }
