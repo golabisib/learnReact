0f56560435cdfd2e37c00460084b6d7d69c1bca9
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-05-18 12:54:20 +0330

    The prerequisite of redux is complete in redux folder

--- redux/index.js
+++ redux/index.js
@@ -1,24 +1,87 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+const combineReducer = redux.combineReducers;
+
+const reduxLogger = require("redux-logger");
+const logger = reduxLogger.createLogger();
+const applyMiddlerWare = redux.applyMiddleware;
+
 // action
 function incrementCounter() {
-    return {
-        type: "INCREMENT_COUNTER",
-    };
+  return {
+    type: "INCREMENT_COUNTER",
+  };
+}
+function decrementCounter() {
+  return {
+    type: "DECREMENT_COUNTER",
+  };
+}
+function incrementCounterByAmount(amount = 1) {
+  return {
+    type: "INCREMENT_COUNTER_BY_AMOUNT",
+    payload: amount,
+  };
+}
+// ------number------
+function incrementNumber() {
+  return {
+    type: "INCREMENT_NUMBER",
+  };
 }
 
-const initialState = {
-    counter: 0,
-    number: 5,
+const initialCounterState = {
+  counter: 0,
+};
+const initialNumberState = {
+  number: 5,
 };
 
 // reducer
-const counterReducer = (state = initialState, action) => {
-    switch (action.type){
-        case "INCREMENT_COUNTER":
-            return {
-                ...state,
-                counter: counter + 1,
-            };
-            default :
-            return state;
-    }
+const counterReducer = (state = initialCounterState, action) => {
+  switch (action.type) {
+    case "INCREMENT_COUNTER":
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+    case "DECREMENT_COUNTER":
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    case "INCREMENT_COUNTER_BY_AMOUNT":
+      return {
+        ...state,
+        counter: state.counter + action.payload,
+      };
+    default:
+      return state;
+  }
 };
+//------number-----
+const numberReducer = (state = initialNumberState, action) => {
+  switch (action.type) {
+    case "INCREMENT_NUMBER":
+      return {
+        ...state,
+        number: state.number + 1,
+      };
+    default:
+      return state;
+  }
+};
+// store
+const rootReducer = combineReducer({
+  counterReducer,
+  numberReducer,
+});
+const store = createStore(rootReducer, applyMiddlerWare(logger));
+
+store.dispatch(incrementCounter());
+
+store.dispatch(decrementCounter());
+
+store.dispatch(incrementCounterByAmount(5));
+
+store.dispatch(incrementNumber());
