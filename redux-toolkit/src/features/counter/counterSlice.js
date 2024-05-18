21d19582e1831ee4b38b2ec97a42e66f6c191c3c
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-05-18 16:49:38 +0330

    The slice an store is created and connected to react whit react-redux

---
+++ redux-toolkit/src/features/counter/counterSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  counterValue: 0,
+};
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.counterValue += 1;
+    },
+    decrement: (state) => {
+      state.counterValue -= 1;
+    },
+  },
+});
+
+export default counterSlice.reducer;
+export const {increment, decrement} = counterSlice.actions;
