2b75027976c6859aaf012b958ad23e9a217e5d79
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-05-18 21:27:26 +0330

    The middleware is added

--- redux-toolkit/src/app/store.js
+++ redux-toolkit/src/app/store.js
@@ -1,13 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import numberReducer from "../features/number/numberSlice";
-
+import logger from "redux-logger";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
     number: numberReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
-export default store
+export default store;
