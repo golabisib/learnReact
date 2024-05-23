def437dcdcbc8eda719ad7e7d8b074623189e35c
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-05-23 23:24:14 +0330

    The connection between React and graphQL is added.

--- ReactGraphQL/src/main.jsx
+++ ReactGraphQL/src/main.jsx
@@ -1,10 +1,20 @@
+import './index.css'
+
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client"
+
+import App from './App.jsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+const client = new ApolloClient({
+    uri: "https://graphqlzero.almansi.me/api",
+    cache: new InMemoryCache(),
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>,
 )
